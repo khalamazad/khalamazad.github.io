c3766d4db2d781b4ae26fe12e69df0354048d5b0
Abul Azad <khalamazad@gmail.com>
2016-02-12 19:43:07 +0530

    done all

--- feedback.php
+++ feedback.php
@@ -1,29 +1,33 @@
 <?php 
  header('Access-Control-Allow-Origin: *');  
+ $postdata = file_get_contents("php://input");
+ $request = json_decode($postdata);
+     
  $to = "khalamazad@gmail.com"; 
  $subject = "From khalamAzad Website Contact Form";
- $name = $_REQUEST['FirstName']; 
- $company = $_REQUEST['LastName']; 
- $email = $_REQUEST['Email']; 
- $phone = $_REQUEST['PhoneNumber'];
- $messagetext = $_REQUEST['message'];
  
-	$message = "Name: ". $name ."\r\n"; 
-	$message .= "Email: ". $email ."\r\n"; 
-	$message .= "Phone: ". $phone ."\r\n"; 
-	$message .= "Company: ". $company ."\r\n";
-	$message .= "Requirement: ". $messagetext ."\r\n";
+ @$FirstName = $request->FirstName;
+ @$LastName = $request->LastName;
+ @$email = $request->Email;
+ @$phone = $request->PhoneNumber;
+ @$messagetext = $request->message;
+
+ $messagetext = $_POST['message'];
+ 
+	$message = "First Name: ". @$FirstName ."\r\n"; 
+	$message .= "Last Name: ". @$LastName ."\r\n";
+	$message .= "Email: ". @$email ."\r\n"; 
+	$message .= "Phone: ". @$phone ."\r\n"; 
+	$message .= "Requirement: ". @$messagetext ."\r\n";
  
  $headers = "From:$email"; 
  $sent = mail($to, $subject, $message, $headers, $message); 
  if($sent) 
  {
-	 echo "<script type='text/javascript'>alert('Your message has been sent. Thank you for contacting us.');</script>";
-
- 
+	 echo "Your message has been sent. Thank you for contacting us.";
  }
  else 
  {
- echo "<script type='text/javascript'>alert('Could not connect to the server. Please try again later.');</script>";
+ 	echo "Could not connect to the server. Please try again later.";
  }
  ?>
