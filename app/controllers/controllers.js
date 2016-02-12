20516e21f3faf6bd583e3aff8e33ded628877c8c
Abul Azad <khalamazad@gmail.com>
2016-02-12 16:20:38 +0530

    updated last

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -76,28 +76,41 @@ myApp.controller('ContactFeedController', function($scope) {
 });
 
 scotchApp.controller('personCtrl', function($scope, $http) {
+    // creating a blank object to hold our form information.
+    //$scope will allow this to pass between controller and view
     $scope.newFeedBack = {};
+    // submission message doesn't show when page loads
+    $scope.submission = false;
+
+    var param = function(data) {
+        var returnString = '';
+        for (d in data){
+            if (data.hasOwnProperty(d))
+                returnString += d + '=' + data[d] + '&';
+        }
+        // Remove last ampersand and return
+        return returnString.slice( 0, returnString.length - 1 );
+    };
+
     $scope.submitForm = function() {
         //return $scope.newFeedBack.userFirstName + " " + $scope.newFeedBack.lastName;
        $http({
             method: 'POST',
             url: 'http://atrad.lk//sitemaster/htmlsite/feedback.php',
-            data: $scope.newFeedBack,  // pass in data as strings
+            data: param($scope.newFeedBack), // pass in data as strings
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
         })
-            .success(function (data) {
-                console.log(data);
-
-                if (!data.success) {
-                    // if not successful, bind errors to error variables
-                    //$scope.errorName = data.errors.name;
-                    //$scope.errorSuperhero = data.errors.superheroAlias;
-                }
-                else {
-                    // if successful, bind success message to message
-                    //$scope.message = data.message;
-
-                }
+           .success(function(data) {
+               if (!data.success) {
+                   // if not successful, bind errors to error variables
+                   $scope.submissionMessage = data.messageError;
+                   $scope.submission = true; //shows the error message
+               } else {
+                   // if successful, bind success message to message
+                   $scope.submissionMessage = data.messageSuccess;
+                   $scope.newFeedBack = {}; // form fields are emptied with this line
+                   $scope.submission = true; //shows the success message
+               }
             });
 
 
--- feedback.php
+++ feedback.php
@@ -1,24 +1,41 @@
 <?php
- $to = "khalamazad@gmail.com";
- $subject = "From KhalamAzad Website Contact Form";
- $name = $_POST['userName'];
- $lname = $_POST['lastName'];
- $email = $_POST['email'];
- $phone = $_POST['phoneNumber'];
- $messagetext = $_POST['message'];
+ $errors = array(); // array to hold validation errors
+ $data = array(); // array to pass back data
+ // return a response ===========================================================
+ // response if there are errors
+ if ( ! empty($errors)) {
+   // if there are items in our errors array, return those errors
+   $data['success'] = false;
+   $data['errors'] = $errors;
+   $data['messageError'] = 'Please check the fields in red';
+ } else {
+   // if there are no errors, return a message
+   $data['success'] = true;
+   $data['messageSuccess'] = 'Hey! Thanks for reaching out. I will get back to you soon';
 
-	$message = "Name: ". $name ."\r\n";
-	$message .= "Last Name: ". $lname ."\r\n";
-	$message .= "Email: ". $email ."\r\n";
-	$message .= "Phone: ". $phone ."\r\n";
-	$message .= "Requirement: ". $messagetext ."\r\n";
+   // CHANGE THE TWO LINES BELOW
+   $email_to = "khalamazad@gmail.com";
+   $email_subject = "From KhalamAzad Website Contact Form";
 
- $headers = "From:$email";
- $sent = mail($to, $subject, $message, $headers, $message);
- if($sent)
- {
-	 print "Your message has been sent. Thank you for contacting Me.";
+   $name = $_POST['userName']; // required
+   $lname = $_POST['lastName']; // required
+   $email_from = $_POST['email']; // required
+   $phone = $_POST['phoneNumber']; // required
+   $message = $_POST['message']; // required
+
+   $email_message = "Feedback details below.nn";
+   $email_message .= "Name: ".$name."n";
+   $email_message .= "Name: ".$lname."n";
+   $email_message .= "Name: ".$phone."n";
+   $email_message .= "Email: ".$email_from."n";
+   $email_message .= "Message: ".$message."n";
+
+   $headers = 'From: '.$email_from."rn".
+   'Reply-To: '.$email_from."rn" .
+   'X-Mailer: PHP/' . phpversion();
+
+   @mail($email_to, $email_subject, $email_message, $headers);
  }
- else
- {print "Could not connect to the server. Please try again later"; }
- ?>
+ // return all our data to an AJAX call
+ echo json_encode($data);
+?>
